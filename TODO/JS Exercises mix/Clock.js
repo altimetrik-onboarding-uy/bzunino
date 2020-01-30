50d2e9084984bec8c7f9c461e519e58ec9aa6021
Bruno Zunino <bzunino@altimetrik.com>
2020-01-30 09:34:48 -0300

    update corrections

--- TODO/JS Exercises mix/Clock.js
+++ TODO/JS Exercises mix/Clock.js
@@ -10,7 +10,7 @@ currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
 
 let timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
 currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
-currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+currentHours = ( currentHours === 0 ) ? 12 : currentHours;
 
 let currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
 
